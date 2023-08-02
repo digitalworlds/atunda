b1ce1600346d1faaa39526569df1c03ef3bdda31
DiegoCarra <forgamesonly057@gmail.com>
2023-08-02 17:29:46 -0400

    Add GIS

--- atunda/frontend/src/App.js
+++ atunda/frontend/src/App.js
@@ -1,19 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
 
 function App() {
-  const [user, setUser] = useState({});
-  
-  if (user.username) {
-    return (
-      <div>Hello {user.username}</div>
-    )
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
   }
-  else {
-    return (
-      <Login setUser={setUser} />
-    )
-  }
+
+  useEffect (() => {
+    /* global google */
+    google.accounts.id.initialize({client_id: "89980881327-61v1jnjbsfdd8rhaoc57shdp77ga2n0f.apps.googleusercontent.com", 
+    callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size:"large"}
+    );
+
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="signInDiv"></div>
+    </div>
+  )
+
 }
 
+  // const [user, setUser] = useState({});
+  
+  // if (user.username) {
+  //   return (
+  //     <div>Hello {user.username}</div>
+  //   )
+  // }
+  // else {
+  //   return (
+  //     <Login setUser={setUser} />
+  //   )
+  // }
+// }
 export default App;
