8ff358dadfebec108a966679d767f57ef63cb3ce
DiegoCarra <forgamesonly057@gmail.com>
2023-08-08 19:31:04 -0400

    Change to sub authentication, clear users

--- atunda/socialauth/views.py
+++ atunda/socialauth/views.py
@@ -25,7 +25,7 @@ class GoogleSignInView(APIView):
                 raise ValueError('Invalid issuer.')
             print(id_info)
             # Check if the user exists in your database based on the email received from Google
-            user, created = User.objects.get_or_create(email=id_info['email'], username=id_info['email'])
+            user, created = User.objects.get_or_create(email=id_info['email'], username=id_info['sub'])
             
             if user.first_name != id_info['given_name']:
                 user.first_name = id_info['given_name']
