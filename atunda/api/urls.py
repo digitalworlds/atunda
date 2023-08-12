996f7639e55277d9507b698a58a6cd7326b41edb
Tristan Pank <panktristan@gmail.com>
2023-08-12 19:06:32 -0400

    Made API for patching a video

--- atunda/api/urls.py
+++ atunda/api/urls.py
@@ -4,7 +4,8 @@ from api import views
 
 urlpatterns = [
     path('users/create/', views.CreateUser.as_view(), name='create_user'),
-    path('video/', views.VideoUploadViewSet.as_view(), name='video_viewset')
+    path('video/update/<int:pk>/', views.UpdateVideoTitle.as_view(), name='update_video'),
+    path('video/', views.VideoUploadViewSet.as_view(), name='video_viewset'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
