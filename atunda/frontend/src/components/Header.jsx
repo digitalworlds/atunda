003be4826a54081026a2e4767652343ec42dca36
DiegoCarra <forgamesonly057@gmail.com>
2023-08-30 13:08:50 -0400

    Make header nicer

--- atunda/frontend/src/components/Header.jsx
+++ atunda/frontend/src/components/Header.jsx
@@ -14,11 +14,15 @@ export default function VideoInput(props) {
 
     return (
         <div className="Header">
-            <h1>Project ATUNDA</h1>
+            <h1 >Project ATUNDA</h1>
             <div className="Success" >{success}</div>
-            {status != 'upload' && <button onClick={handleUpload}>Upload</button>}
-            {status != 'profile' && <button onClick={handleProfile}>Profile</button>}
-            <h1>{user.first_name + " " +  user.last_name}</h1>
+            {status != 'upload' && 
+                <h1 class='page-button' onClick={handleUpload}>Upload</h1>
+            }
+            {status != 'profile' && 
+                <h1 class='page-button' onClick={handleProfile}>Profile</h1>
+            }
+            {/* <h1>{user.first_name + " " +  user.last_name}</h1> */}
             <img className="Profile" src={user.profile_pic}></img>
         </div>
     )
